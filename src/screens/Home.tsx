260ba0d982935098a68c4935de1bb4c2c5150be0
vinicius.pereira <viniibp@gmail.com>
2022-03-16 16:19:47 -0300

    init expo app eartquake_viewer

---
+++ src/screens/Home.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text } from 'react-native';
+import { Container } from './styles';
+
+export const Home = () => {
+
+  return (
+    <Container>
+      <Text>oi</Text>
+    </Container>
+  );
+};
+
+export default Home;
