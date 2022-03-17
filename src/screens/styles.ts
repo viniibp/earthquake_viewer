977da1ef4880ca380d09aa8dc105662fa68d9075
vinicius.pereira <viniibp@gmail.com>
2022-03-17 13:35:42 -0300

    commited

--- src/screens/styles.ts
+++ src/screens/styles.ts
@@ -2,4 +2,19 @@ import styled from "styled-components/native";
 
 export const Container = styled.ScrollView`
   flex: 1;
+  height: 100%;
+  background-color: ${({ theme }) => theme.COLORS.BACKGROUND};
+  padding: 25px 6px;
+`;
+
+export const Header = styled.View`
+  height: 100px;
+  flex-direction: row;
+  flex: 1;
+`;
+
+export const OrderOption = styled.TouchableOpacity`
+  flex: 1;
+  margin: 5px;
+  background-color: paleturquoise;
 `;
--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,15 +1,10 @@
 export default {
   COLORS: {
-    BACKGROUND: '#F2F2F2',
+    BACKGROUND: '#3B0C59',
     TEXT: '#170126',
-    ALERT: '#73074D',
+    ALERT: '#F2F2F2',
 
     PRIMARY_900: '#280540',
     PRIMARY_800: '#3B0C59'
   },
-  FONTS: {
-    REGULAR: 'Roboto_400Regular',
-    MEDIUM: 'Roboto_500Medium',
-    BOLD: 'Roboto_700Bold'
-  }
 };
